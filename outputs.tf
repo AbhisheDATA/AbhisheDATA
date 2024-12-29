fb35651dcb70453df5a9ef0e17f24bb9f56ab35c
Abhishek kumar <abhisheklinkd5@gmail.com>
2024-12-29 18:02:21 +0530

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "project_id" {
-  value       = google_project.main.project_id
-  description = "The ID of the created project"
-}
-
-output "project_number" {
-  value       = google_project.main.number
-  description = "The project number"
-}
